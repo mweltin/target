6c2d0ded77dfe432dcc118406615a0738e5a4c37
mweltin <mweltin@pattan.net>
2023-12-27 19:49:48 -0800

    main add laplacian

--- canny.py
+++ canny.py
@@ -7,6 +7,7 @@ edges = cv.Canny(img,100,200)
 cv.imshow('canny edges', edges)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
 # plt.subplot(121),plt.imshow(img,cmap = 'gray')
 # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 # plt.subplot(122),plt.imshow(edges,cmap = 'gray')
---
+++ laplacian.py
@@ -0,0 +1,34 @@
+import cv2
+
+# Load the image
+img = cv2.imread('data/IMG_0042.jpeg')
+lower = img.copy()
+
+# Create a Gaussian Pyramid
+gaussian_pyr = [lower]
+for i in range(3):
+   lower = cv2.pyrDown(lower)
+   gaussian_pyr.append(lower)
+
+# Last level of Gaussian remains same in Laplacian
+laplacian_top = gaussian_pyr[-1]
+
+# Create a Laplacian Pyramid
+laplacian_pyr = [laplacian_top]
+for i in range(3,0,-1):
+   size = (gaussian_pyr[i - 1].shape[1], gaussian_pyr[i - 1].shape[0])
+   gaussian_expanded = cv2.pyrUp(gaussian_pyr[i], dstsize=size)
+   laplacian = cv2.subtract(gaussian_pyr[i-1], gaussian_expanded)
+   laplacian_pyr.append(laplacian)
+
+# create three windows to display three layers of images
+cv2.namedWindow('Layer 1', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Layer 2', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Layer 3', cv2.WINDOW_AUTOSIZE)
+
+# display all three layers
+cv2.imshow('Layer 1',laplacian_pyr[3])
+cv2.imshow('Layer 2',laplacian_pyr[2])
+cv2.imshow('Layer 3',laplacian_pyr[1])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
