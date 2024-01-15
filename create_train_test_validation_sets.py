9f164072df03215ae9ce1af80fd4af0ca511a65f
Your Name <you@example.com>
2024-01-14 22:11:16 -0800

    add parse args to cttv

--- create_train_test_validation_sets.py
+++ create_train_test_validation_sets.py
@@ -1,9 +1,21 @@
 import os
+import argparse
+import glob
+
+parser = argparse.ArgumentParser(
+                    prog='cttv',
+                    description='Create a train, test and validation sets',
+                    epilog='')
+parser.add_argument('--location', help='list of directories to find images and label files', action='store', default='./' )
+parser.add_argument('--train_per', help='precentage of files to use as a train set', default=45, action='store', type=int)   
+parser.add_argument('--test_per', help='percentage of files to use for train set', default=45, action='store', type=int)    
+parser.add_argument('--validate_per', help='precentage of files to use for validation set', default=10, action='store', type=int)  
+parser.add_argument('--lable_extension', choices['xml', 'json'], default='xml', action='store', help='file extension for lables' )  
 
 
-def main():
+def main(args):
     # configurable options
-    img_root = os.path.join('Tensorflow', 'workspace', 'images', 'collectedimages')
+    img_root = os.path(args.location) 
 
     extract_data_from = {
         'LIVELONG': os.path.join(img_root, 'collectedimages', 'livelong'),
@@ -18,17 +30,16 @@ def main():
         'VALIDATION': os.path.join(img_root, 'validation'),
     }
 
-    # if using labelImg.py label_format='xml', if using labelMe.py label_format='json'
-    label_format = 'xml'
-
     # end configurable options
 
     # get file list. this will be used so we don't add the same file to two different directories
     dir_list = {}
     for key, path in extract_data_from:
-        dir_list[key] = os.listdir(path)
+        files = glob.glob(os.path.join(path,'*.'+args.lable_extension))
+        print(files)
 
-
+    
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    main(args)
